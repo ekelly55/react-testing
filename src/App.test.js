595063e370737c7ee082c25b07f4faf64860e1c4
ekelly55 <eamonn.michael.kellyr@email.com>
2022-12-05 14:44:49 -0500

    initial app render test works. working on mockFetch

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import mockFetch from "./mocks/mockFetch";
 import App from "./App";
 
-test("renders the landing page", () => {
-  render(<App />);
+beforeEach(() => {
+  jest.spyOn(window, "fetch").mockImplementation(mockFetch);
+})
+
+afterEach(()=> {
+  jest.restoreAllMocks()
 });
