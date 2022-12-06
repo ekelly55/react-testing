3ce44dab5d0e6e2cb95102de2ccbcb0294daef86
ekelly55 <eamonn.michael.kellyr@email.com>
2022-12-05 22:09:17 -0500

    test work

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,16 @@ import { render, screen } from "@testing-library/react";
 import mockFetch from "./mocks/mockFetch";
 import App from "./App";
 
+test('renders the landing page', async q() => {
+  render(<App />);
+  
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+  expect(await screen.findByRole("option", { name: "husky"})).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
+  expect(screen.getByRole("img")).toBeInTheDocument();
+});
+
 beforeEach(() => {
   jest.spyOn(window, "fetch").mockImplementation(mockFetch);
 })
